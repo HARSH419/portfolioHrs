00f396c1b492d83f0e2cbbf68c5b2e5009c6be7b
Harsh Singhal <Harshsinghal24dec@gmail.com>
2022-12-11 20:27:59 +0530

    .

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,28 +1,31 @@
 import { Container } from "./styles";
 import emailIcon from "../../assets/email-icon.svg";
-import phoneIcon from "../../assets/phone-icon.svg"
+import phoneIcon from "../../assets/phone-icon.svg";
 import { Form } from "../Form/Form";
 
-
-export function Contact(){
-
-  return(
+export function Contact() {
+  return (
     <Container id="contact">
       <header>
-        <h2>Entre em contact comigo</h2>
-        <p>Se você viu o meu potencial ou quer conversar comigo, não exite em me mandar uma mensagem.</p>
+        <h2>Contact me</h2>
+        <p>
+          If you saw my potential or want to talk to me, don't hesitate to send
+          me a message.
+        </p>
       </header>
       <div className="contacts">
         <div>
           <img src={emailIcon} alt="Email" />
-          <a href="mailto:harshdeveloper369@gmail.com">harshdeveloper369@gmail.com</a>
+          <a href="mailto:harshdeveloper369@gmail.com">
+            harshdeveloper369@gmail.com
+          </a>
         </div>
         <div>
           <img src={phoneIcon} alt="Email" />
           <a href="tel:+5531981213843">(+91) 8700326959</a>
-        </div>  
+        </div>
       </div>
       <Form></Form>
     </Container>
-  )
+  );
 }
